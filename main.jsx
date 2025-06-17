3b3d03e652e65555d41d9753d25e242c8b45248b
PerrinT31 <thomas.perrin@toptex.fr>
2025-06-17 08:19:09 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.jsx"
+// main.jsx
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";  // Charge les styles globaux (fonts, background, etc.)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
+
